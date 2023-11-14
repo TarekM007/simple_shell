8eaefaeed206f0912e25cd1fe351820963b5f584
TarekM007 <tarekmoustafa36@gmail.com>
2023-11-14 14:07:25 -0800

    uploading shell project

---
+++ shell.h
@@ -0,0 +1,26 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/wait.h>
+
+#define DELIM " \t\n"
+
+extern char **environ;
+
+void shell_interactive(void);
+void shell_no_interactive(void);
+char *read_line(void);
+char **split_line(char *line);
+int execute_args(char **args);
+int new_process(char **args);
+char *read_stream(void);
+int own_cd(char **args);
+int own_exit(char **args);
+int own_env(char **args);
+int own_help(char **args);
+
+#endif
